c5cf55cbf36470a01deefc87a15076d9b4c73de5
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-07-22 18:53:01 +0300

    Updates

---
+++ precache-manifest.106a790ac84631925b2376aca503224c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49abee0081dab5637cdaf64e7978191b",
+    "url": "/goit-react-hw-pixabay/index.html"
+  },
+  {
+    "revision": "fa2bd164fda5403f36b5",
+    "url": "/goit-react-hw-pixabay/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "2567c541bf3705ea1dc8",
+    "url": "/goit-react-hw-pixabay/static/css/main.9e1a062b.chunk.css"
+  },
+  {
+    "revision": "fa2bd164fda5403f36b5",
+    "url": "/goit-react-hw-pixabay/static/js/2.517437f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-pixabay/static/js/2.517437f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2567c541bf3705ea1dc8",
+    "url": "/goit-react-hw-pixabay/static/js/main.9ba6de5b.chunk.js"
+  },
+  {
+    "revision": "b3cfec21f38e407a216b",
+    "url": "/goit-react-hw-pixabay/static/js/runtime-main.1c19ab69.js"
+  }
+]);
